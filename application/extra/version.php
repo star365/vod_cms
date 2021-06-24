83ff60d49ea78e801bfbb72864f8522fad15927f
wangguoqiang <guoqiangwang@163.com>
2021-06-24 10:47:28 +0800

    2020.1000.1075

---
+++ application/extra/version.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+    'name' => '苹果CMS内容管理系统',
+    'copyright' => 'MacCMS',
+    'url' => '//github.com/magicblack',
+    'code' => '2020.1000.1075',
+    'license' => '免费版',
+);
+?>
---
+++ application/index/controller/Test.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+
+class Test extends Base
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+         echo 'test';
+    }
+
+}
